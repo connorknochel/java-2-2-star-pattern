d0d17d29e510cdda28c2d6448ebed222a186ea25
Justin Kruskie <kruskiej@baisd.net>
2024-02-24 16:41:52 +0000

    Update

--- StarPattern.java
+++ StarPattern.java
@@ -1,7 +1,5 @@
-public class StarPattern
-{
-    public static void main(String[] args)
-    {
+public class StarPattern {
+    public static void main(String[] args) {
         // Display the pattern below.
 
     }
