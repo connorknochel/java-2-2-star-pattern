8cf8f80cf4e8781c303a4c2b8893d9da07ca3b44
connorknochel <knochcon2027@gmail.com>
2025-09-10 10:02:08 -0400

    made a star design

--- StarPattern.java
+++ StarPattern.java
@@ -2,7 +2,11 @@ public class StarPattern {
 
     public static void main(String[] args) {
         // Display the pattern below.
-
+            System.out.println("       *");
+            System.out.println("      ***");
+            System.out.println("  ***********");
+            System.out.println("    *******");
+            System.out.println("   **** ****"); 
+            System.out.println("  **       **");
     }
-    
 }
